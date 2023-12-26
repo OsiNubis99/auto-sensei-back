fa8bf28b085979f69a5ab62f20fc53982c260b4d
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2023-12-26 19:13:59 -0400

    fix: Common service export

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,7 +3,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@database/database.module';
 import { MailerModule } from 'src/mailer/mailer.module';
 
+import AWSService from './aws/service';
+import TwilioService from './twilio/service';
+
+const commonServices = [AWSService, TwilioService];
+
 @Module({
   imports: [DatabaseModule, MailerModule],
+  providers: [...commonServices],
+  exports: [...commonServices],
 })
 export class CommonModule {}
