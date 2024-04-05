029147c994b393c98477e99c49ccacf6565613bf
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2024-04-05 14:09:38 -0400

    feature: New pdf test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,39 @@
-import VinDecoderService from '@common/services/vin-decoder.service';
-import { Controller, Get, Param } from '@nestjs/common';
+import AWSService from '@common/services/aws.service';
+import PDFService from '@common/services/pdf.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
+import * as htmtest from 'html-pdf-node';
+
+class Test {
+  @IsOptional()
+  @ApiProperty({
+    description: 'Seller data',
+    required: false,
+  })
+  text: string;
+}
 
 @Controller()
 export class AppController {
-  constructor(private de: VinDecoderService) {}
+  constructor(private de: PDFService, private awsService: AWSService) {}
 
   @Get()
   getHello() {
     return 'Hi';
   }
 
-  @Get('/vin/:vin')
-  getVin(@Param('vin') vin: string) {
-    return this.de.getCarData(vin);
+  @Post('/test')
+  async getVin(@Body() { text }: Test) {
+    const file = {
+      content: text,
+    };
+    const options = { format: 'A4' };
+    const doc = await htmtest.generatePdf(file, options);
+    return await this.awsService.upload(
+      'auction/contract',
+      'aeuoeuaoeu-not-signed.pdf',
+      doc,
+    );
   }
 }
