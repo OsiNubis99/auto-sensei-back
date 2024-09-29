cf92e96b3b404586a470aa68a79e888eb4774047
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2024-09-29 15:25:37 -0400

    feat: disable vin function

--- src/common/services/vin-decoder.service.ts
+++ src/common/services/vin-decoder.service.ts
@@ -1,14 +1,14 @@
-import { Either } from '@common/generics/either';
+// import { Either } from '@common/generics/either';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import axios from 'axios';
-import * as CryptoJS from 'crypto-js';
-
-import {
-  TrimOptions,
-  VehicleDetailsI,
-} from '@database/interfaces/vehicle-details.interface';
-import { DriveTrainEnum } from '@common/enums/drive-train.enum';
+// import axios from 'axios';
+// import * as CryptoJS from 'crypto-js';
+//
+// import {
+//   TrimOptions,
+//   VehicleDetailsI,
+// } from '@database/interfaces/vehicle-details.interface';
+// import { DriveTrainEnum } from '@common/enums/drive-train.enum';
 
 @Injectable()
 export default class VinDecoderService {
@@ -24,82 +24,82 @@ export default class VinDecoderService {
     this.secret = configService.get('decoded-vin.secret');
   }
 
-  async getCarData(vin: string) {
-    const date = Date.now().toString();
-    const nonce = 'asd' + date;
-    const secretDigestUnencrypted = nonce + date + this.secret;
-    const secretDigest = CryptoJS.SHA1(secretDigestUnencrypted).toString(
-      CryptoJS.enc.Base64,
-    );
-
-    const Authorization =
-      'Atmosphere realm="' +
-      this.host +
-      '",chromedata_app_id="' +
-      this.appId +
-      '",chromedata_nonce="' +
-      nonce +
-      '",chromedata_secret_digest="' +
-      secretDigest +
-      '",chromedata_digest_method="SHA1",chromedata_version="1.0",chromedata_timestamp="' +
-      date +
-      '"';
-
-    try {
-      const { data } = await axios
-        .get(this.url + vin, {
-          headers: { Authorization },
-        })
-        .catch((err) => err);
-
-      if (!data) return Either.makeLeft(new Error('Api error'));
-
-      const result = data.result || {};
-
-      let transmission: string = undefined;
-      let cylinder: string = undefined;
-
-      for (const feature of result.features) {
-        if (feature.description == 'Engine Cylinders') {
-          cylinder = feature.name;
-        }
-        if (feature.description == 'Transmission') {
-          transmission = feature.name;
-        }
-      }
-
-      const trimOptions = result.vehicles?.map(
-        (item) =>
-          <TrimOptions>{
-            trim: item.styleDescription || item.trim || '',
-            doors: item.doors || '',
-            driveTrain: DriveTrainEnum[item.driveType + ''],
-            bodyType: item.bodyType || '',
-          },
-      ) || [
-        <TrimOptions>{
-          trim: '',
-          doors: '',
-          driveTrain: DriveTrainEnum.AWD,
-          bodyType: '',
-        },
-      ];
-
-      return Either.makeRight(<VehicleDetailsI>{
-        vin,
-        year: result.year,
-        basePrice: 100,
-        make: result.make,
-        model: result.model,
-        transmission,
-        cylinder,
-        suggestedPrice: 100,
-
-        trimOptions,
-        ...trimOptions[0],
-      });
-    } catch (err) {
-      return Either.makeLeft(new Error('Vin is invalid'));
-    }
-  }
+  // async getCarData(vin: string) {
+  //   const date = Date.now().toString();
+  //   const nonce = 'asd' + date;
+  //   const secretDigestUnencrypted = nonce + date + this.secret;
+  //   const secretDigest = CryptoJS.SHA1(secretDigestUnencrypted).toString(
+  //     CryptoJS.enc.Base64,
+  //   );
+  //
+  //   const Authorization =
+  //     'Atmosphere realm="' +
+  //     this.host +
+  //     '",chromedata_app_id="' +
+  //     this.appId +
+  //     '",chromedata_nonce="' +
+  //     nonce +
+  //     '",chromedata_secret_digest="' +
+  //     secretDigest +
+  //     '",chromedata_digest_method="SHA1",chromedata_version="1.0",chromedata_timestamp="' +
+  //     date +
+  //     '"';
+  //
+  //   try {
+  //     const { data } = await axios
+  //       .get(this.url + vin, {
+  //         headers: { Authorization },
+  //       })
+  //       .catch((err) => err);
+  //
+  //     if (!data) return Either.makeLeft(new Error('Api error'));
+  //
+  //     const result = data.result || {};
+  //
+  //     let transmission: string = undefined;
+  //     let cylinder: string = undefined;
+  //
+  //     for (const feature of result.features) {
+  //       if (feature.description == 'Engine Cylinders') {
+  //         cylinder = feature.name;
+  //       }
+  //       if (feature.description == 'Transmission') {
+  //         transmission = feature.name;
+  //       }
+  //     }
+  //
+  //     const trimOptions = result.vehicles?.map(
+  //       (item) =>
+  //         <TrimOptions>{
+  //           trim: item.styleDescription || item.trim || '',
+  //           doors: item.doors || '',
+  //           driveTrain: DriveTrainEnum[item.driveType + ''],
+  //           bodyType: item.bodyType || '',
+  //         },
+  //     ) || [
+  //       <TrimOptions>{
+  //         trim: '',
+  //         doors: '',
+  //         driveTrain: DriveTrainEnum.AWD,
+  //         bodyType: '',
+  //       },
+  //     ];
+  //
+  //     return Either.makeRight(<VehicleDetailsI>{
+  //       vin,
+  //       year: result.year,
+  //       basePrice: 100,
+  //       make: result.make,
+  //       model: result.model,
+  //       transmission,
+  //       cylinder,
+  //       suggestedPrice: 100,
+  //
+  //       trimOptions,
+  //       ...trimOptions[0],
+  //     });
+  //   } catch (err) {
+  //     return Either.makeLeft(new Error('Vin is invalid'));
+  //   }
+  // }
 }
